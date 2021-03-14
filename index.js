1859333d367da535810e75d91e189550b7188f27
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-14 17:00:25 -0500

    tweaks bootstrap, puts photos in grid, earthdates not in grid yet

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const endPointEarthDates = "http://localhost:3000/api/v1/earthdates"
 const endPointPhotos = "http://localhost:3000/api/v1/photos"
 
@@ -7,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(earthdates => {
         console.log(earthdates);
         createEarthDates(earthdates);
-        console.log(earthdates);
         createPhotos(earthdates);
     });
 });
@@ -15,33 +16,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function createEarthDates(earthdates) {
    const ed_div = document.querySelector("#ed");
+   ed_div.className = "container-fluid";
+   const ed_row = document.createElement("div");
+   ed_row.className = "row"
    earthdates["data"].forEach((d) => {
     const ed_container = document.createElement("div");
-    const ed_header = document.createElement('h3');
+    ed_container.className = "col-sm"
+    const ed_header = document.createElement('h4');
     const ed_p = document.createElement('p')
     ed_header.innerText = d["attributes"]["date"];
     ed_p.innerText = `Available Photos: ${d["attributes"]["total_photos"]}`;
     ed_container.appendChild(ed_header);
     ed_container.appendChild(ed_p);
-    ed_div.appendChild(ed_container);
+    ed_row.appendChild(ed_container);
+    ed_div.appendChild(ed_row);
    })
     
 }
 
 function createPhotos(earthdates) {
     const photos_div = document.querySelector("#photos");
+    photos_div.className = "container-fluid";
     earthdates["data"].forEach((d) => {
+        const earthdate_row = document.createElement("div");
+        earthdate_row.className = "row"
+        earthdate_row.id = d["id"];
         d["attributes"]["photos"].forEach((p) => {
             const photo_container = document.createElement("div");
             const sol = document.createElement("p");
             const img_elem = document.createElement("img");
             sol.innerText = `Sol: ${p["sol"]}`;
             img_elem.src = p["img_src"];
-            photo_container.className = "photo_container"
+            img_elem.className = "img-fluid img-thumbnail";
+            photo_container.className = "col-2 col-md-4"
             photo_container.id = p["id"]
             photo_container.appendChild(img_elem);
             photo_container.appendChild(sol);
-            photos_div.appendChild(photo_container);
+            earthdate_row.appendChild(photo_container);
+            photos_div.appendChild(earthdate_row);
         })
     })
 }
