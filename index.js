571438cd00763a7f5045b0c3a2b55f810faee435
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-17 01:48:46 -0500

    adds like functionality and pop out window for photos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const endPointEarthDates = "http://localhost:3000/api/v1/earthdates"
 const endPointPhotos = "http://localhost:3000/api/v1/photos"
 const emptyHeart = "♡" 
-const filledHeart = "♥"
+const fullHeart = "♥"
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch(endPointEarthDates)
@@ -20,7 +20,7 @@ function createEarthDates(earthdates) {
    const ed_div = document.querySelector("#ed");
    ed_div.className = "container-fluid";
    const ed_row = document.createElement("div");
-   ed_row.className = "row d-flex justify-content-center"
+   ed_row.className = "row d-flex justify-content-evenly"
    ed_row.id = "override"
    earthdates["data"].forEach((d) => {
         const button = document.createElement('div');
@@ -40,6 +40,25 @@ function createEarthDates(earthdates) {
     
 }
 
+function addHeartEvent(element, photo_id, current_likes) {
+    element.addEventListener('click', () => {
+        fetch(endPointPhotos + '/' + photo_id, {
+            method: 'PATCH',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({photo_id: photo_id, likes: element["id"]})
+        })
+        
+        if(element["id"] == "empty") {
+            element.innerText = fullHeart + ` Total likes: ${current_likes += 1}`;
+            element["id"] = "full";
+        } else {
+            element.innerText = emptyHeart + ` Total likes: ${current_likes -= 1}`;
+            element["id"] = "empty";
+        }
+        console.log(photo_id)
+    })
+}
+
 function createPhotos(earthdates) {
     const photos_div = document.querySelector("#photos");
     photos_div.className = "container-fluid";
@@ -51,10 +70,11 @@ function createPhotos(earthdates) {
         earthdate_row_header.innerText = d["attributes"]["date"];
         earthdate_row_header.className = "text-center";
         earthdate_row.appendChild(earthdate_row_header);
-        d["attributes"]["photos"].forEach((p) => {
+        d["attributes"]["photos"].sort((a, b) => parseFloat(b["likes"]) - parseFloat(a["likes"])).forEach((p) => {
             const photo_container = document.createElement("div");
             const sol = document.createElement("p");
             const heart = document.createElement("p");
+            addHeartEvent(heart, p["id"], p["likes"]);
             const img_elem = document.createElement("img");
             heart.id = "empty"
             heart.innerText = emptyHeart + ` Total likes: ${p["likes"]}`;
@@ -63,6 +83,7 @@ function createPhotos(earthdates) {
             img_elem.className = "img-fluid img-thumbnail";
             photo_container.className = "col-1 col-md-2"
             photo_container.id = p["id"]
+            addPopOutEvent(photo_container, p["img_src"])
             photo_container.appendChild(img_elem);
             photo_container.appendChild(heart)
             photo_container.appendChild(sol);
@@ -72,8 +93,28 @@ function createPhotos(earthdates) {
     })
 }
 
-function patchFetch(photo_id) {
-    fetch(endPointPhotos + '/' + photo_id, () => {
-        METHOD: "PATCH"
+function addPopOutEvent(element, img_src) {
+    element.addEventListener('click', () => {
+        openWindow(img_src);
     })
+}
+
+let w = 480, h = 340;
+function openWindow(img_src){
+if (document.getElementById) {
+   w = screen.availWidth;
+   h = screen.availHeight;
+}  
+
+const popW = 800, popH = 700;
+
+const leftPos = (w-popW)/2;
+const topPos = (h-popH)/2;
+
+msgWindow = window.open('','popup','width=' + popW + ',height=' + popH + ',top=' + topPos + ',left=' + leftPos + ',       scrollbars=yes');
+msgWindow.document.write 
+    ('<HTML><HEAD><TITLE>Perseverance Rover Photo</TITLE></HEAD><BODY><FORM    NAME="photo">' +
+    `<img src=${img_src}>` +
+    ' Click the button below to close the window.<br />' +
+    '<INPUT TYPE="button" VALUE="X"onClick="window.close();"></FORM></BODY>   </HTML>');
 }
